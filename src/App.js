99cf64f328187d61a91c2212c7c01fdf88f1eada
Sanjeev Kumar <sanjeev.kumar@tothenew.com>
2020-05-26 10:47:17 +0530

    using the useState() Hook for State manipulation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
 
@@ -28,35 +28,61 @@ import Person from './Person/Person'
 //   );
 // }
 
-class App extends Component{
-  state = {
+const App = propes =>{
+
+  const [personState, setPersonState] = useState({
     persons: [
       { name:"Max", age:28 },
       { name:"John", age:27 },
       { name:"Manu", age:24 }
-    ]
-  }
+    ]//,
+    // otherState : "some other value" 
+  });
 
-  personHandler = () => {
-    this.setState({
+  // we can have as many useSatate  
+  const [otherState, setOtherState] = useState('some other value')
+
+  //console.log(personState);
+  console.log(personState, otherState);
+
+  const personHandler = () => {
+    setPersonState({
       persons: [
         { name:"Maximo", age:28 },
         { name:"John", age:27 },
         { name:"Manu", age:30 }
       ]
-    })
-  }
+      //,otherState : personState.otherState
+    });
+  };
 
-  render(){
+// here when we click on button personState object i.e.
+// persons: [
+//   { name:"Max", age:28 },
+//   { name:"John", age:27 },
+//   { name:"Manu", age:24 }
+// ],
+// otherState : "some other value" +
+// will replaced by setPersonState i.e.
+// persons: [
+//   { name:"Maximo", age:28 },
+//   { name:"John", age:27 },
+//   { name:"Manu", age:30 }
+// ]
+// means it does't merge them to still be there otherState we need to add :
+// otherState : personState.otherState
+
     return (
       <div className="App">
-        <button onClick={this.personHandler}>Click to Change</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+        <button onClick={personHandler}>Click to Change</button>
+        <Person name={personState.persons[0].name} age={personState.persons[0].age}></Person>
+        <Person name={personState.persons[1].name} age={personState.persons[1].age}></Person>
+        <Person name={personState.persons[2].name} age={personState.persons[2].age}></Person>
       </div>
     )
   }
-}
+
 
 export default App;
+
+
