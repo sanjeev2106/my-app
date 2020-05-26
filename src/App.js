d7cf6d9d4a5dd872413e47db0343fc865d501629
Sanjeev Kumar <sanjeev.kumar@tothenew.com>
2020-05-26 13:11:29 +0530

    passing method as a reference

--- src/App.js
+++ src/App.js
@@ -1,88 +1,47 @@
-import React, { useState } from 'react';
-//import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//         <Person name="Sanjeev" age="26" />
-//         <Person name="Shubham" age="27"> My hobbie is Trouble Shooting</Person>
-//         <Person name="Rohan" age="23" />
-//       </header>
-      
-//     </div>
-//   );
-// }
-
-const App = propes =>{
-
-  const [personState, setPersonState] = useState({
+class App extends Component{
+  state = {
     persons: [
       { name:"Max", age:28 },
       { name:"John", age:27 },
       { name:"Manu", age:24 }
-    ]//,
-    // otherState : "some other value" 
-  });
+    ]
+  }
 
-  // we can have as many useSatate  
-  const [otherState, setOtherState] = useState('some other value')
-
-  //console.log(personState);
-  console.log(personState, otherState);
-
-  const personHandler = () => {
-    setPersonState({
+  personHandler = () => {
+    this.setState({
       persons: [
         { name:"Maximo", age:28 },
         { name:"John", age:27 },
         { name:"Manu", age:30 }
       ]
-      //,otherState : personState.otherState
-    });
-  };
+    })
+  }
 
-// here when we click on button personState object i.e.
-// persons: [
-//   { name:"Max", age:28 },
-//   { name:"John", age:27 },
-//   { name:"Manu", age:24 }
-// ],
-// otherState : "some other value" +
-// will replaced by setPersonState i.e.
-// persons: [
-//   { name:"Maximo", age:28 },
-//   { name:"John", age:27 },
-//   { name:"Manu", age:30 }
-// ]
-// means it does't merge them to still be there otherState we need to add :
-// otherState : personState.otherState
-
+  render(){
     return (
       <div className="App">
-        <button onClick={personHandler}>Click to Change</button>
-        <Person name={personState.persons[0].name} age={personState.persons[0].age}></Person>
-        <Person name={personState.persons[1].name} age={personState.persons[1].age}></Person>
-        <Person name={personState.persons[2].name} age={personState.persons[2].age}></Person>
+        <button onClick={this.personHandler}>Click to Change</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}>
+        </Person>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          // pass method as reference
+          click={this.personHandler}>
+        </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}>
+        </Person>
       </div>
     )
   }
-
+}
 
 export default App;
-
-
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const person = (propes) => {
     return (
-        <div>
+        <div onClick={propes.click}>
             <p>I'm {propes.name} and my age is {propes.age}!</p>
             <p>{propes.children}</p>
         </div>
