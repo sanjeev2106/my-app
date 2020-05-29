a414fb5fe2dc79264e00642e1f7f873cd4a3a92d
Sanjeev Kumar <sanjeev.kumar@tothenew.com>
2020-05-29 17:06:36 +0530

    using Radium for Media Queries, missed to apply style previously

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -8,7 +8,7 @@ const person = (props) => {
 };
 
     return (
-        <div className="Person" onClick={props.click}>
+        <div className="Person" style={style} onClick={props.click}>
             <p>I'm {props.name} and my age is {props.age}!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
