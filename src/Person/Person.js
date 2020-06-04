213ea348279eb6d2e5838c6889c09455c8e1ebeb
Sanjeev Kumar <sanjeev.kumar@tothenew.com>
2020-06-04 17:28:08 +0530

    css modules and media queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,35 +1,31 @@
 import React from 'react';
 import styled from 'styled-components'
+import classes from './Person.css'
 
-const StyledDiv = styled.div`  
-  width: 60%;
-  margin: 20px auto;
-  border: 1px solid #eef;
-  padding: 16px;
-  text-align: center;
-  box-shadow: 0 2px 3px #ccc; 
+// const StyledDiv = styled.div`  
+//   width: 60%;
+//   margin: 20px auto;
+//   border: 1px solid #eef;
+//   padding: 16px;
+//   text-align: center;
+//   box-shadow: 0 2px 3px #ccc; 
 
-  @media (min-width: 500px) {
-    width:'450px';
-}
-`;
+//   @media (min-width: 500px) {
+//     width:'450px';
+// }
+// `;
 
 const person = (props) => {
-    const style = {
-    '@media (min-width: 500px)': {
-        width:'450px'
-    }
-};
-
     return (
         //<div className="Person" style={style} onClick={props.click}>
-        <StyledDiv>
+        <div className={classes.Person}>
             <p>I'm {props.name} and my age is {props.age}!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
-        </StyledDiv>
+        </div>
         
     )
 };
+
 
 export default person;
