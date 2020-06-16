e179f932ba3a39d30d0596c508ec7e023470dbaf
Sanjeev Kumar <sanjeev.kumar@tothenew.com>
2020-06-16 13:33:18 +0530

    Rendering adjacent JSX Elements by using Hood Aux

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import classes from './Person.css';
+//import classes from './Person.css';
+import Aux from '../../../hoc/Aux'
 
 
 class Person extends Component {
@@ -17,14 +18,14 @@ class Person extends Component {
         //     </div>        
         // )
 
-        return [
-            //<div className="Person" style={style} onClick={props.click}>
-                
+        return (
+            <Aux>  
             <p key="i1">I'm {this.props.name} and my age is {this.props.age}!</p>,
             <p key="i2">{this.props.children}</p>,
             <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
-                   
-        ];
+            </Aux>
+      
+        );
     }   
 };
 
---
+++ src/hoc/Aux.js
@@ -0,0 +1,5 @@
+//import React from 'react'; // not realy used here because we are not using jsx code here
+
+const aux = props => props.children;
+
+export default aux;
