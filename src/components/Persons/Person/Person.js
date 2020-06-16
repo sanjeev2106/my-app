b1c33d1565136b8d150fb3a3cbab74b95d62e102
Sanjeev Kumar <sanjeev.kumar@tothenew.com>
2020-06-16 13:22:38 +0530

    Rendering adjacent JSX Elements by returning in array form without wrapping to a root element

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,14 +5,26 @@ import classes from './Person.css';
 class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
-        return (
-        //<div className="Person" style={style} onClick={props.click}>
-            <div className={classes.Person}>
-                <p>I'm {this.props.name} and my age is {this.props.age}!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>        
-        )
+        // As know, we can return only a single element means we have a root element and others are inside that.
+        // In this case div is root element. But React provide a way where we don't neet to wrap in to root element
+        // but nee to return these as a array with unique element key. 
+        // return (
+        // //<div className="Person" style={style} onClick={props.click}>
+        //     <div className={classes.Person}>
+        //         <p>I'm {this.props.name} and my age is {this.props.age}!</p>
+        //         <p>{this.props.children}</p>
+        //         <input type="text" onChange={this.props.changed} value={this.props.name}/>
+        //     </div>        
+        // )
+
+        return [
+            //<div className="Person" style={style} onClick={props.click}>
+                
+            <p key="i1">I'm {this.props.name} and my age is {this.props.age}!</p>,
+            <p key="i2">{this.props.children}</p>,
+            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
+                   
+        ];
     }   
 };
 
